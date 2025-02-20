786aca04a7f785b2745e472558d2f8ce8b254063
Veto <vetobastiero@gmail.com>
2025-02-20 17:07:45 +0800

    fix: revert changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,2 @@
-import { handlers, signIn, signOut, auth } from "@/auth"; // Referring to the auth.ts we just created
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
-export { signIn, signOut, auth };
