2cf46b4737ec8519b17994eeb0d457c8e040d8f8
Veto <vetobastiero@gmail.com>
2025-02-20 17:03:03 +0800

    fix: adds exports for other fns

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+import { handlers, signIn, signOut, auth } from "@/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
+export { signIn, signOut, auth };
